8dd12c7d0f4da2a910ded28f20a3d2e162a6e7eb
truejojo <johannesveh@yahoo.de>
2025-05-19 15:01:45 +0200

    ButtonGroup with map

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -1,12 +1,20 @@
 import Button from './Button';
 
+const buttons = [
+  { id: '1', text: 'Mark to list' },
+  { id: '2', text: 'Mark all as incomplete' },
+  { id: '3', text: 'Reset to initial' },
+  { id: '4', text: 'Remove all items' },
+];
+
 const ButtonGroup = () => {
   return (
     <section className='button-group'>
-      <Button type='secondary'>Mark to list</Button>
-      <Button type='secondary'>Mark all as incomplete</Button>
-      <Button type='secondary'>Reset to initial</Button>
-      <Button type='secondary'>Remove all items</Button>
+      {buttons.map((button) => (
+        <Button key={button.id} type='secondary'>
+          {button.text}
+        </Button>
+      ))}
     </section>
   );
 };
